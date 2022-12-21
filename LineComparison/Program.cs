7e5ebfad2ee3b46b71e9d96d0486d83908ff2880
pkr187 <pankajroman187@gmail.com>
2022-12-22 01:42:59 +0530

    [pankaj] Add:CalculateItsLength

---
+++ LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparison comparision = new Comparison(5, 6, 8, 9);
+            comparision.CalculateLength();
+        }
+    }
+}
