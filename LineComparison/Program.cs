e42944b7eae39f990c8472b7bcd26fe4e31dd757
pkr187 <pankajroman187@gmail.com>
2022-12-22 01:57:53 +0530

    [pankaj] Add:ChecktwolinesAreEqualsOrNot

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Comparison comparision = new Comparison(5, 6, 8, 9);
-            comparision.CalculateLength();
+            Comparison comparision = new Comparison(1, 3, 5, 8);
+            double line1 = comparision.CalculateLength();
+            Console.WriteLine("Length of first line " + line1);
+            Comparison LineComparision = new Comparison(5, 6, 8, 9);
+            double line2 = LineComparision.CalculateLength();
+            Console.WriteLine("Length of second line " + line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("line1 is equal to line2");
+            }
+            else
+            {
+                Console.WriteLine("line1 is not equal to line2");
+            }
         }
     }
 }
